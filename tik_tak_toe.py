eb5957411a97f2c3d1b1a993cf7a986063782b7c
Felix Iltgen <felixiltgen@hotmail.de>
2024-12-29 17:05:03 +0100

    Added start_game fuction

--- tik_tak_toe.py
+++ tik_tak_toe.py
@@ -1,6 +1,7 @@
 import numpy as num
 import os
 import random
+import msvcrt as m
 
 game_field=[["-","-","-"],
             ["-","-","-"],
@@ -9,20 +10,32 @@ current_player = bool
 player_zero = 0
 player_one = 1
 
+
+
+def start_game():
+    os.system("cls")
+    print("Drücke eine Taste, um das Spiel zu Starten...")
+    print("----------------------------------------")
+    m.getch() 
+    choose_player() 
+
 def choose_player():
     print("Die Spielreihenfolge wird zufällig zugewiesen!")
     random_int = random.randint(0,1)
     if (random_int == 0):
         print("Spiler O beginnt!")
+        print_game_field()
+        #Weiter mit erstem Spielzug von O
     else:
         print("Spieler X beginnt")
-
+        print_game_field()
+        #Weiter mit erstem Spielzug von X
 
 #prints all tree rows of the gamfield list
 def print_game_field():
     
     #delete previous printed fields 
-    os.system("cls") # => not all fields only specified ones
+    #os.system("cls") # => not all fields only specified ones
 
     print(f"{game_field[0][0]} | {game_field[0][1]} | {game_field[0][2]}")
     print(f"--+---+--")
@@ -30,4 +43,5 @@ def print_game_field():
     print(f"--+---+--")
     print(f"{game_field[2][0]} | {game_field[2][1]} | {game_field[2][2]}")
 
-choose_player()
+start_game()
+
