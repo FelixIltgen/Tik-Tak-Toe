10e898ddde34c7a2f1b132eecfb944baa725ff3f
Felix Iltgen <felixiltgen@hotmail.de>
2024-12-29 14:33:25 +0100

    finished progress

--- tik_tak_toe.py
+++ tik_tak_toe.py
@@ -1,6 +1,18 @@
 import numpy
+import os
 
+game_field = [["X","-","-"],
+          ["-","O","-"],
+          ["-","-","X"]]
 
+#prints all tree rows of the gamfield list
+def print_game_field():
+    #delete previous printed fields 
+    os.system("cls") # => not all fields only specified ones
 
-fields = []
-print("Hello")
+    print(f"{game_field[0][0]} | {game_field[0][1]} | {game_field[0][2]}")
+    print(f"--+---+--")
+    print(f"{game_field[1][0]} | {game_field[1][1]} | {game_field[1][2]}")
+    print(f"--+---+--")
+    print(f"{game_field[2][0]} | {game_field[2][1]} | {game_field[2][2]}")
+print_game_field()
